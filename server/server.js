b2ed425e03dba537c0a3b12cbf0c1a38b70397cc
yeshoham <yeshoham@cisco.com>
2018-11-10 16:29:34 +0200

    Add unique names per room and case insensitive rooms

--- server/server.js
+++ server/server.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
+
 var users = new Users();
 
 app.use(express.static(publicPath));
@@ -25,14 +26,18 @@ io.on('connection', (socket) => {
         if (!isRealString(params.name) || !isRealString(params.room)) {
             return callback('Name and room name are required.');
         }
+        var userInRoom = users.getUserList(params.room.toUpperCase());
+        if (userInRoom.includes(params.name)) {
+            return callback('A user by that name is already in the room.');
+        }
 
-        socket.join(params.room);
+        socket.join(params.room.toUpperCase());
         users.removeUser(socket.id);
-        users.addUser(socket.id, params.name, params.room);
+        users.addUser(socket.id, params.name, params.room.toUpperCase());
 
-        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+        io.to(params.room.toUpperCase()).emit('updateUserList', users.getUserList(params.room.toUpperCase()));
         socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
-        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
+        socket.broadcast.to(params.room.toUpperCase()).emit('newMessage', generateMessage('Admin', `${params.name} has joined`));
         callback();
     });
 
@@ -68,3 +73,11 @@ io.on('connection', (socket) => {
 server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
+
+// New feature ideas : 
+// chat rooms - case insensitive
+// user names - unique 
+// list of currently active rooms available - dropdown - populates like people list populates
+//
+//
+
